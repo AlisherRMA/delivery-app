bd1e3fea8b10ed4cd17d6af136e1ad9b05098da9
Alisher <ashimkhan@yandex.kz>
2021-05-18 19:29:33 +0600

    app: added history-api-fallback

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
-const express = require("express");
-const serveStatic = require("serve-static");
-const path = require("path");
+var express = require("express");
+var history = require("connect-history-api-fallback");
+var app = express();
 
-const app = express();
+// Middleware for serving '/dist' directory
+const staticFileMiddleware = express.static("dist");
 
-app.use("/", serveStatic(path.join(__dirname, "/dist")));
+// 1st call for unredirected requests
+app.use(staticFileMiddleware);
 
-const port = process.env.PORT || 8080;
-app.listen(port);
+// Support history api
+app.use(history({}));
 
-console.log("listening on port " + port);
+// 2nd call for redirected requests
+app.use(staticFileMiddleware);
+
+app.listen(3000, function() {
+  console.log("Example app listening on port 3000!");
+});
